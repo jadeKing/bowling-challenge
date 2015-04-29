ff70dd9cb93b5aa873925e8d6041b7752b011ba5
Jade King <dev@jadeking.co.uk>
2015-04-29 01:21:14 +0100

    add increses score

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -3,12 +3,13 @@ describe("Scorecard", function(){
   beforeEach(function(){
     scorecard = new Scorecard();
   });
-  it("has a 0 score to start", function(){
+  it("has 0 score to start", function(){
     expect(scorecard.score).toEqual(0);
   });
-  // it("increases score by given amount", function(){
-  //   expect(scorecard.increaseScore(5)).toEqal
-  // });
+  it("increases score by given amount", function(){
+    scorecard.addScore(5);
+    expect(scorecard.score).toEqual(5);
+  });
 
 
 });
--- src/js/Scorecard.js
+++ src/js/Scorecard.js
@@ -1,3 +1,7 @@
 var Scorecard = function(){
   this.score = 0;
 };
+
+Scorecard.prototype.addScore = function(score){
+  this.score += score;
+};
